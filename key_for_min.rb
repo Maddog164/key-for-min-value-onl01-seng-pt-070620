811d0bf2cdc10532b453184ebd0ac7239c165fb6
Denise Henderson <denise@henderson3.us>
2020-07-16 02:13:26 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,16 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+i = 1
+name_hash.each do |key, value|
+  if 
+    elsif i = 1
+      min_value=value
+      answer_key = ""
+    elsif  value < min_value
+      min_value = value
+      answer_key = key
+    end #if 
+    return answer_key   
+  end #do
 end
